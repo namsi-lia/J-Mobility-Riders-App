1b935a0f23106bc34c681512f4b92fd09668b370
briangathua <brian.gathua@icealion.com>
2024-04-03 19:21:56 +0300

    initial setup

---
+++ android/app/src/main/kotlin/com/jmobility/jmobility_riders/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jmobility.jmobility_riders
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/ui/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors {
+  Color colorPrimary = const Color(0xffE87722);
+  Color colorSecondary = const Color(0xff0C9F3E);
+  Color lightColorSecondary = const Color(0xffCAFFDD);
+  Color buttonGreyColor = const Color(0xff8996A3);
+  Color dangerRedColor = const Color(0xffBB1600);
+  Color deliveryStatusColor = const Color(0xff9747FF);
+  Color lightDeliveryStatusColor = const Color(0xffE8D6FF);
+}
